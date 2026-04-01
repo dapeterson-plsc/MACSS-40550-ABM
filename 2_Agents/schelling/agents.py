c1299e6808f2ca6686dd16d1d448c8ed549b2443
dapeterson-plsc <dapeterson@uchicago.edu>
2026-04-01 16:46:06 -0500

    Complete code for Schelling agent

--- 2_Agents/schelling/agents.py
+++ 2_Agents/schelling/agents.py
@@ -5,21 +5,21 @@ class SchellingAgent(Agent):
     def __init__(self, model, agent_type):
         super().__init__(model)
         ## Set agent type
-        self.type = ________
+        self.type = agent_type
     ## Define basic decision rule
     def move(self):
         ## Get list of neighbors within range of sight
         neighbors = self.model.grid.get_neighbors(
-            _______________________________)
+            self.pos, moore = True, radius = self.model.radius, include_center = False)
         ## Count neighbors of same type as self
-        similar_neighbors = _________________________
+        similar_neighbors = len([n for n in neighbors if n.type == self.type])
         ## If an agent has any neighbors (to avoid division by zero), calculate share of neighbors of same type
-        if ____________________
-            share_alike = ______________________
+        if (valid_neighbors := len(neighbors)) > 0:
+            share_alike = similar_neighbors / valid_neighbors
         else:
-            _________________
+            share_alike = 0
         ## If unhappy with neighbors, move to random empty slot. Otherwise add one to model count of happy agents.
-        if share_alike < __________________:
-            self.model.grid.move_to_empty(_____)
+        if share_alike < self.model.desired_share_alike:
+            self.model.grid.move_to_empty(self)
         else: 
-            _______________  
+            self.model.happy +=1   
